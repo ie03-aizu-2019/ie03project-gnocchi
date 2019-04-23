c395c68f1a32a2ce9008890830ac1a72097c21af
ShujiOba <shuji365630@gmail.com>
2019-04-23 09:58:40 +0900

    :+1: Add Stringable interface

--- back/model/model.go
+++ back/model/model.go
@@ -1,6 +1,13 @@
 package model
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
+
+type Stringable interface {
+	ToString() string
+}
 
 type Point struct {
 	X float64
@@ -26,4 +33,20 @@ type Query struct {
 
 func (p *Point) Length() float64 {
 	return math.Sqrt(p.X*p.X + p.Y*p.Y)
+}
+
+func (p Point) ToString() string {
+	return fmt.Sprintf("%.5f %.5f", p.X, p.Y)
+}
+
+func (p Place) ToString() string {
+	return p.Coord.ToString()
+}
+
+func (r Road) ToString() string {
+	return fmt.Sprintf("%d %s %s", r.Id, r.From.Id, r.To.Id)
+}
+
+func (q Query) ToString() string {
+	return fmt.Sprintf("%s %s %d", q.Start, q.Dest, q.Num)
 }
