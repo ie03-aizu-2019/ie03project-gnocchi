fc5eddcd787878f68dcd25a0950e18c5af4b08f6
schktjm <si2.o.mjt@gmail.com>
2019-05-02 17:12:27 +0900

    WIP

--- back/phase1/task3.go
+++ back/phase1/task3.go
@@ -1,5 +1,18 @@
 package phase1
 
+import (
+	"fmt"
+
+	"github.com/uzimaru0000/ie03project-gnocchi/back/model"
+)
+
+func calcShortedtPath(roads []*model.Road) float64 {
+	for _, road := range roads {
+		fmt.Printf(road)
+	}
+	return 0
+}
+
 func dijkstra() float64 {
 	return 0
 }
