6c38efb7c3f85aa671837eae19a61e6c40e37aaa
schktjm <si2.o.mjt@gmail.com>
2019-06-22 17:22:19 +0900

    WIP

--- back/phase2/task5.go
+++ back/phase2/task5.go
@@ -1,1 +1,151 @@
 package phase2
+
+import (
+	"container/heap"
+	"log"
+	"reflect"
+
+	"github.com/uzimaru0000/ie03project-gnocchi/back/utils"
+
+	"github.com/uzimaru0000/ie03project-gnocchi/back/model"
+)
+
+type Item struct {
+	roads    []*model.Road
+	priority float64
+	index    int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+// we want the ascending-prioirty queue
+// so we use less than here
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq PriorityQueue) Push(x interface{}) {
+	n := len(pq)
+	item := x.(*Item)
+	item.index = n
+	pq = append(pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) update(item *Item, roads []*model.Road, priority float64) {
+	item.roads = roads
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
+
+func avoidPlaces(base []*model.Place, avoids []*model.Road) (result []*model.Place) {
+	for _, bs := range base {
+		flg := true
+		for _, av := range avoids {
+			// danger????
+			if reflect.DeepEqual(bs, av.To) || reflect.DeepEqual(bs, av.From) {
+				flg = false
+				break
+			}
+		}
+		if flg {
+			result = append(result, bs)
+		}
+	}
+	return
+}
+
+func avoidRoads(base []*model.Road, avoids []*model.Road) (result []*model.Road) {
+	for _, bs := range base {
+		flg := true
+		for _, av := range avoids {
+			if reflect.DeepEqual(bs.To, av.To) || reflect.DeepEqual(bs.To, av.From) || reflect.DeepEqual(bs.From, av.To) || reflect.DeepEqual(bs.From, av.From) {
+				flg = false
+				break
+			}
+		}
+		if flg {
+			result = append(result, bs)
+		}
+	}
+	return
+}
+
+func joinRoads(spurRoads []*model.Road, roads [][]*model.Road) (result [][]*model.Road) {
+	for _, rs := range roads {
+		result = append(result, append(spurRoads, rs...))
+	}
+	return
+}
+
+func roadsLen(rs []*model.Road) (result float64) {
+	for _, r := range rs {
+		result += r.Length()
+	}
+	return
+}
+
+func calcKthShortestPath(q model.Query, places []*model.Place, roads []*model.Road) (result [][]*model.Road) {
+	k := q.Num
+	var start, dest *model.Place
+	for _, p := range places {
+		if q.Start == p.Id {
+			start = p
+		}
+		if q.Dest == p.Id {
+			dest = p
+		}
+	}
+
+	shortests := utils.Dijkstra(start, places, roads)[*dest]
+	pq := make(PriorityQueue, len(shortests))
+	for i, ss := range shortests {
+		pq[i] = &Item{
+			roads:    ss,
+			priority: roadsLen(ss),
+			index:    i,
+		}
+	}
+	heap.Init(&pq)
+
+	for pq.Len() > 0 || len(result) < k {
+		baseItem, ok := pq.Pop().(*Item)
+		baseRoute := baseItem.roads
+		if !ok {
+			log.Print("pop failed")
+		}
+		result = append(result, baseRoute)
+		spurNode := start
+		spurRoads := []*model.Road{}
+
+		for i, r := range baseRoute {
+			notWalk := r
+			dijkPoints := avoidPlaces(places, spurRoads)
+			if i != 0 {
+				dijkPoints = append(dijkPoints, spurNode) // avoidPlace で取り除くと初めの点もなくなるから
+
+			}
+			dijkRoads := avoidRoads(roads, spurRoads)
+
+		}
+
+	}
+
+	return
+}
--- back/phase2/task5_test.go
+++ back/phase2/task5_test.go
@@ -2,8 +2,10 @@ package phase2
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 
+	"github.com/uzimaru0000/ie03project-gnocchi/back/model"
 	"github.com/uzimaru0000/ie03project-gnocchi/back/phase1"
 	"github.com/uzimaru0000/ie03project-gnocchi/back/utils"
 )
@@ -46,4 +48,88 @@ func task5(file string) string {
 
 func TestTask5Case1(t *testing.T) {
 	utils.Assert("phase2/task5/case1", task5, t)
+}
+
+func TestAvoidPlaces(t *testing.T) {
+	places := []*model.Place{
+		&model.Place{"0", model.Point{0, 0}},
+		&model.Place{"1", model.Point{1, 0}},
+		&model.Place{"2", model.Point{0, 1}},
+		&model.Place{"3", model.Point{0, 2}},
+		&model.Place{"4", model.Point{2, 2}},
+	}
+
+	avoid := []*model.Road{
+		&model.Road{2, places[1], places[2]},
+		&model.Road{3, places[2], places[3]},
+	}
+
+	anser := []*model.Place{
+		&model.Place{"0", model.Point{0, 0}},
+		&model.Place{"4", model.Point{2, 2}},
+	}
+
+	result := avoidPlaces(places, avoid)
+
+	if !reflect.DeepEqual(result, anser) {
+		t.Fatal("test avoidPlace failed!")
+	}
+}
+
+func TestAvoidRoads(t *testing.T) {
+	places := []*model.Place{
+		&model.Place{"0", model.Point{0, 0}},
+		&model.Place{"1", model.Point{1, 0}},
+		&model.Place{"2", model.Point{0, 1}},
+		&model.Place{"3", model.Point{0, 2}},
+		&model.Place{"4", model.Point{2, 2}},
+	}
+	roads := []*model.Road{
+		&model.Road{0, places[0], places[1]},
+		&model.Road{1, places[0], places[2]},
+		&model.Road{2, places[1], places[2]},
+		&model.Road{3, places[2], places[3]},
+		&model.Road{4, places[3], places[4]},
+	}
+
+	avoid := []*model.Road{
+		&model.Road{1, places[0], places[2]},
+		&model.Road{2, places[1], places[2]},
+	}
+
+	anser := []*model.Road{
+		&model.Road{4, places[3], places[4]},
+	}
+
+	result := avoidRoads(roads, avoid)
+
+	if !reflect.DeepEqual(result, anser) {
+		t.Fatal("test avoidRoads railed!")
+	}
+}
+
+func TestJoinRoads(t *testing.T) {
+	places := []*model.Place{
+		&model.Place{"0", model.Point{0, 0}},
+		&model.Place{"1", model.Point{1, 0}},
+		&model.Place{"2", model.Point{0, 1}},
+		&model.Place{"3", model.Point{0, 2}},
+		&model.Place{"4", model.Point{2, 2}},
+	}
+
+	roads := []*model.Road{
+		&model.Road{0, places[0], places[1]},
+		&model.Road{1, places[0], places[2]},
+		&model.Road{2, places[1], places[2]},
+		&model.Road{3, places[2], places[3]},
+	}
+
+	tail := [][]*model.Road{
+		roads[2:],
+	}
+
+	result := joinRoads(roads[:2], tail)
+	if !reflect.DeepEqual(roads, result[0]) {
+		t.Fatal("test joinRoads failed!")
+	}
 }
