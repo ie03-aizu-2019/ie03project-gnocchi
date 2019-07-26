20efd967dd3b15900e211cf6001ff6679a6ae3aa
schktjm <si2.o.mjt@gmail.com>
2019-07-27 01:57:12 +0900

    feature task6

---
+++ back/phase2/task6_test.go
@@ -0,0 +1,47 @@
+package phase2
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/uzimaru0000/ie03project-gnocchi/back/phase1"
+	"github.com/uzimaru0000/ie03project-gnocchi/back/utils"
+)
+
+func task6(file string) string {
+
+	str, err := utils.Load(file)
+	if err != nil {
+		return err.Error()
+	}
+
+	datas, err := utils.ParseData(str)
+	if err != nil {
+		return err.Error()
+	}
+
+	roads := phase1.ConnectOnRoadPoints(datas.Roads, datas.Places)
+
+	roads, places := phase1.EnumerateCrossPoints(roads)
+
+	result := ""
+	for _, q := range datas.Queries {
+		routes := calcKthShortestPath(*q, append(datas.Places, places...), roads)
+		if len(routes) == 0 {
+			result += fmt.Sprintln("NA")
+			continue
+		}
+		for _, rs := range routes {
+			sum := 0.0
+			for _, r := range rs {
+				sum += r.Length()
+			}
+			result += fmt.Sprintf("%s\n%s\n", toFormat(sum), pathToString(rs, q.Start))
+		}
+	}
+	return result
+}
+
+func TestTask6Case1(t *testing.T) {
+	utils.Assert("phase2/task6/case1", task6, t)
+}
